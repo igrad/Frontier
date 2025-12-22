139f781150e4c25465336e330fb6353a54bebe01
Ian Gradert <iangradert@gmail.com>
2025-12-22 12:26:21 -0600

    Revised library structure for UI

---
+++ src/UI/CoreUI/Wallpaper/WallpaperView.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <QObject>
+
+namespace Wallpaper
+{
+// TODO: Change QObject here to a widget or frame or something
+class WallpaperView: public QObject
+{
+   Q_OBJECT
+
+};
+
+}
